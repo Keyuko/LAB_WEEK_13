395c942026e3fdc42c90478296116f70bfeb6f72
Keyuko <devind3d3@gmail.com>
2025-12-09 22:39:22 +0700

    Commit No. 1 – add data binding

--- app/src/main/java/com/example/lab_week_13/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_13/MainActivity.kt
@@ -1,18 +1,14 @@
-package com.example.test_lab_week_12
+package com.example.lab_week_13
 
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.RecyclerView
-import com.example.test_lab_week_12.model.Movie
-import com.google.android.material.snackbar.Snackbar
-import kotlinx.coroutines.launch
-import java.util.Calendar
+import com.example.lab_week_13.model.Movie
+import com.example.lab_week_13.databinding.ActivityMainBinding
+import androidx.databinding.DataBindingUtil
 
 class MainActivity : AppCompatActivity() {
     private val movieAdapter by lazy {
@@ -25,7 +21,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil
+            .setContentView(this, R.layout.activity_main)
         val recyclerView: RecyclerView = findViewById(R.id.movie_list)
         recyclerView.adapter = movieAdapter
         val movieRepository = (application as MovieApplication).movieRepository
@@ -35,33 +32,8 @@ class MainActivity : AppCompatActivity() {
                     return MovieViewModel(movieRepository) as T
                 }
             })[MovieViewModel::class.java]
-        // fetch movies from the API
-// lifecycleScope is a lifecycle-aware coroutine scope
-        lifecycleScope.launch {
-// repeatOnLifecycle is a lifecycle-aware coroutine builder
-// Lifecycle.State.STARTED means that the coroutine will run
-// when the activity is started
-            repeatOnLifecycle(Lifecycle.State.STARTED) {
-                launch {
-// collect the list of movies from the StateFlow
-                    movieViewModel.popularMovies.collect {
-// add the list of movies to the adapter
-                            movies ->
-                        movieAdapter.addMovies(movies)
-                    }
-                }
-                launch {
-// collect the error message from the StateFlow
-                    movieViewModel.error.collect { error ->
-// if an error occurs, show a Snackbar with the error message
-                        if (error.isNotEmpty()) Snackbar
-                            .make(
-                                recyclerView, error, Snackbar.LENGTH_LONG
-                            ).show()
-                    }
-                }
-            }
-        }
+        binding.viewModel = movieViewModel
+        binding.lifecycleOwner = this
     }
 
     private fun openMovieDetails(movie: Movie) {
--- app/src/main/java/com/example/lab_week_13/api/MovieService.kt
+++ app/src/main/java/com/example/lab_week_13/api/MovieService.kt
@@ -1,5 +1,5 @@
-package com.example.test_lab_week_12.api
-import com.example.test_lab_week_12.model.PopularMoviesResponse
+package com.example.lab_week_13.api
+import com.example.lab_week_13.model.PopularMoviesResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
 
